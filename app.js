6e5493359291b71042045ac75d50d31549a03cc2
Hypoxic <martini_a@outlook.com>
2024-12-01 03:59:02 -0700

    update

--- app.js
+++ app.js
@@ -5,14 +5,16 @@
 document.getElementById("generate_button").addEventListener("click", processOutput);
 
 // Set labels for boxes
-const playerNameInputBoxLabel = "Player Names (One Per Line)";
+const playerNameInputBoxLabel = "Player Names";
 document.getElementById("playerNameInputBoxLabel").textContent = playerNameInputBoxLabel;
 
-const roleInputBoxLabel = "Squads And Roles (Squad starts with # - One Per Line)";
+const roleInputBoxLabel = "Squads And Roles";
 document.getElementById("roleInputBoxLabel").textContent = roleInputBoxLabel;
 
 // Set boxes with default data
 const playerNames = [
+    "// Enter each player's name on a separate line",
+    "",
     "Emma",
     "Noah",
     "Lila",
@@ -26,6 +28,8 @@ const playerNameString = playerNames.join("\n");
 document.getElementById("playerNameInputBox").value = playerNameString;
 
 const roles = [
+    "// Create squads using the '#' symbol",
+    "",
     "#Alpha Squad",
     "Squad Leader",
     "Grenadier",
@@ -44,15 +48,22 @@ document.getElementById("availableClassesInputBox").value = roleString;
 // Load saved data from localStorage
 loadSavedData();
 
+// Process Initial Output
+processOutput();
+
 // FUNCTIONS
 //------------------------------------------------------------------------------------------------
+function isCommentOrEmpty(line) {
+    return line.startsWith("//") || line.trim() === "";
+}
+
 function parseNames(inputText) {
     let lines = inputText.split("\n")
     let names = [];
 
     for (let i = 0; i < lines.length; i++) {
         const trimmedLine = lines[i].trim();
-        if (trimmedLine !== "") {
+        if (!(trimmedLine.startsWith("//", 0) || trimmedLine === "")) {
             names.push(trimmedLine);
         }
     }
@@ -68,9 +79,12 @@ function parseRoles(inputText) {
 
     for (let i = 0; i < lines.length; i++) {
         const trimmedLine = lines[i].trim();
-        if (trimmedLine.startsWith("#")) {
-            squads.push(trimmedLine.substring(1).trim());
+        if (isCommentOrEmpty(trimmedLine)) {
             continue;
+        }
+        if (trimmedLine.startsWith("#")) {
+            const trimmed = trimmedLine.substring(1).trim();
+            squads.push(trimmed);
         }
         roles.push(trimmedLine)
     }
@@ -101,10 +115,19 @@ function processOutput() {
     const roles = rolesAndSquads[1];
 
     // Exit early if there are uneven roles and names
-    if (names.length !== roles.filter(item => item !== "").length) {
+    if (names.length !== roles.filter(item => item !== "" && !item.startsWith("#")).length) {
+        const roleCount = roles.filter(item => item !== "" && !item.startsWith("#")).length;
         console.log("Uneven amount of names and roles!");
         console.log("Names:", names, names.length)
-        console.log("Roles:", roles, roles.length);
+        console.log("Roles:", roles, roleCount);
+
+        Swal.fire({
+            icon: "error",
+            title: "Uneven Names and Roles!",
+            html: `Names: ${names.length}<br>Roles: ${roleCount}`,
+            width: "70%",
+            background: "#000000",
+        });
         return;
     }
 
@@ -121,9 +144,9 @@ function processOutput() {
     });
 
     // Assign names to roles
-    let squadIndex = 0;
+    let squadIndex = -1;
     roles.forEach((role, index) => {
-        if (role === "") {
+        if (role.startsWith("#")) {
             squadIndex++;
             return;
         }
@@ -143,7 +166,7 @@ function processOutput() {
     // Build the output string
     let squadString = "";
 
-    const squadKeys = Object.keys(squadDictionary).reverse();
+    const squadKeys = Object.keys(squadDictionary);
 
     for (const squad of squadKeys) {
         if (squadString !== "") {
